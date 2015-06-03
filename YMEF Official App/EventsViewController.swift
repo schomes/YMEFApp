7cb1d338619052c50ce239a361943a4fc98c3fbc
tphanco <phan.d.tt@gmail.com>
2015-06-02 20:44:08 -0500

    Update logo bar on event page

--- YMEF Official App/EventsViewController.swift
+++ YMEF Official App/EventsViewController.swift
@@ -21,15 +21,13 @@ class EventsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(animated: Bool) {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        imageView.contentMode = .ScaleAspectFit
+        
+        let image = UIImage(named: "logo_YMEF")
+        imageView.image = image
+        navigationItem.titleView = imageView
     }
-    */
-
 }
