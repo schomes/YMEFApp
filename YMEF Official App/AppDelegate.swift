5483abe7cad212f5bea6ac38ae07a96d5ae57324
tphanco <phan.d.tt@gmail.com>
2015-07-28 15:15:30 -0500

    Delete tab bar, begin working on nav bar

--- YMEF Official App/AppDelegate.swift
+++ YMEF Official App/AppDelegate.swift
@@ -30,12 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         var navigationBarAppearance = UINavigationBar.appearance()
         navigationBarAppearance.barTintColor = uicolorFromHex(0x273845)
         
-        var tabBarAppearance = UITabBar.appearance()
-        tabBarAppearance.barTintColor = uicolorFromHex(0x273845)
-        
-        var tabButtonAppearance = UITabBar.appearance()
-        tabButtonAppearance.tintColor = uicolorFromHex(0xFFFFFF)
-        
         return true
     }
 
