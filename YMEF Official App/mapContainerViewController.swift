98f0ef1f80edb909c11aa0519c889d7ad4d3fb2d
David Schommer <djschomes@gmail.com>
2015-08-05 23:37:34 -0500

    add 'current location' button and introduce moving map to current location when prompted

--- YMEF Official App/mapContainerViewController.swift
+++ YMEF Official App/mapContainerViewController.swift
@@ -8,22 +8,87 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class mapContainerViewController: UIViewController {
+class mapContainerViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBAction func showCurrentLocation(sender: AnyObject) {
+//        var currentLocation = 
+//        centerMapOnLocation(location: currentLocation)
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.startUpdatingLocation()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let initialLocation = CLLocation(latitude: 21.282778, longitude: -157.829444)
+        centerMapOnLocation(initialLocation)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    let regionRadius: CLLocationDistance = 1000
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+            regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    // MARK: - location manager to authorize user location for Maps app
+    var locationManager = CLLocationManager()
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        checkLocationAuthorizationStatus()
+        
+        // set-up for current location
+        self.locationManager.delegate = self
+        
+        self.mapView.showsUserLocation = true
+        
+    }
+    
+    
+    // MARK: - Delegate Methods for Location
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        
+        centerMapOnLocation(location)
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        print("Errors: \(error.localizedDescription)")
+    }
+    
+    
+    
+    
     
 
     /*
