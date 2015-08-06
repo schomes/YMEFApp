08664b42e2e1509b6874e9dc96c7f823afbfcc47
David Schommer <djschomes@gmail.com>
2015-08-05 20:33:00 -0500

    add map view and tabs

---
+++ YMEF Official App/mapContainerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  mapContainerViewController.swift
+//  YMEF Official App
+//
+//  Created by David Schommer on 8/5/15.
+//  Copyright (c) 2015 YMEF. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class mapContainerViewController: UIViewController {
+
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
