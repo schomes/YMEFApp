a5f50312ecdf11f2c4b63563121ba50202169801
David Schommer <djschomes@gmail.com>
2015-08-07 15:41:03 -0500

    add jsonHandler.swift file to fetch and parse json files

---
+++ YMEF Official App/jsonHandler.swift
@@ -0,0 +1,12 @@
+//
+//  jsonHandler.swift
+//  YMEF Official App
+//
+//  Created by David Schommer on 8/7/15.
+//  Copyright (c) 2015 YMEF. All rights reserved.
+//
+
+// fetch and parse JSON files
+
+import Foundation
+
